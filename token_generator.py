214bdbb306f5a4df1f155ac49af5fc6893627672
eyadnawar <72501081+eyadnawar@users.noreply.github.com>
2021-06-14 00:14:40 +0200

    Update token_generator.py

--- token_generator.py
+++ token_generator.py
@@ -4,8 +4,12 @@ import random
 def generate_tokens(file_path, numOfChars):
     file_path = file_path
     numOfChars = numOfChars
-
-    file = open(file_path, 'w')
+    
+    try:
+        file = open(file_path, 'w')
+        
+    except:
+        print("error, could not create file.")
 
     for i in range (0, int(1e7)):
         file.write("".join(random.choices(string.ascii_lowercase, k = numOfChars)) + '\n')
