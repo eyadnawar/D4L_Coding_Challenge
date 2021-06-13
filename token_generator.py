a6e5457f28c57a6487b268e80002104087697749
eyadnawar <72501081+eyadnawar@users.noreply.github.com>
2021-06-14 00:09:05 +0200

    Update token_generator.py

--- token_generator.py
+++ token_generator.py
@@ -3,20 +3,14 @@ import random
 from token_reader_HashSet import token_reader_hashset
 from token_reader_sorting import token_reader_sorted_tokens
 
-def generate_tokens():
-    file_path = 'C:/Users/Lenovo/Desktop/D4L.txt'
-    s = 7
-    random_string = "".join(random.choices(string.ascii_lowercase, k = s))
+def generate_tokens(file_path, numOfChars):
+    file_path = file_path
+    numOfChars = numOfChars
 
     file = open(file_path, 'w')
 
     for i in range (0, int(1e7)):
-        file.write("".join(random.choices(string.ascii_lowercase, k = 7)) + '\n')
+        file.write("".join(random.choices(string.ascii_lowercase, k = numOfChars)) + '\n')
 
     file.close()
     return
-
-if __name__ == "__main__":
-    generate_tokens()
-    token_reader_hashset()
-    token_reader_sorted_tokens()
