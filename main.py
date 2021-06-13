a62d0bcdad2f500cfabfbc6f9cfdd977795c53a4
eyadnawar <72501081+eyadnawar@users.noreply.github.com>
2021-06-14 00:11:23 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+
+
+if __name__ == "__main__":
+  file_path = 'C:/Users/Lenovo/Desktop/D4L.txt'
+  numOfChars = 7
+  generate_tokens(file_path, numOfChars)
+  token_reader_hashset(file_path)
+  token_reader_sorted_tokens(file_path)
