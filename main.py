57f79f9f0d7ea4b0a5d883662f61bd0c97ed87d9
eyadnawar <72501081+eyadnawar@users.noreply.github.com>
2021-06-14 00:12:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-
+from token_reader_HashSet import token_reader_hashset
+from token_reader_sorting import token_reader_sorted_tokens
 
 if __name__ == "__main__":
   file_path = 'C:/Users/Lenovo/Desktop/D4L.txt'
